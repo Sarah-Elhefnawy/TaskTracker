66ae6fd4fa07b52076ef199a691e0966c9a9b884
Sarah Elhefnawy <sarahelhefnawy0@gmail.com>
2024-09-05 19:00:54 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿namespace TaskTracker
+﻿using System.Collections;
+
+namespace TaskTracker
 {
 	internal class Program
 	{
+		static string[] tasks = new string[100];
+
+		static int taskIndex = 0;
 		static void Main(string[] args)
 		{
 			//Welcome user
@@ -12,42 +17,88 @@
 			//5. Exit
 
 			Console.WriteLine("Welcome to my task tracker");
-			Console.WriteLine("Enter your choice from 1 to 5");
+			Console.WriteLine("------------------------");
 
-			string userChoice = Console.ReadLine();
 
-			switch (userChoice)
+			while (true)
 			{
-				case "1":
-
-					AddTask();
-					break;
-				case "2":
-
-
-					break;
-				case "3":
+				Console.WriteLine("Enter your choice from 1 to 5");
+				Console.WriteLine("1. Add");
+				Console.WriteLine("2. View");
+				Console.WriteLine("3. Complete");
+				Console.WriteLine("4. Remove");
+				Console.WriteLine("5. Exit");
 
 
-					break;
-				case "4":
+				string userChoice = Console.ReadLine();
 
-
-					break;
-				case "5":
-
-
-					break;
-				default:
-					Console.WriteLine("Please enter number from 1 to 5 only");
-					break;
+				switch (userChoice)
+				{
+					case "1":
+						AddTask();
+						break;
+					case "2":
+						ViewTasks();
+						break;
+					case "3":
+						MarkComplete();
+						break;
+					case "4":
+						DeleteTask();
+						break;
+					case "5":
+						Environment.Exit(0);
+						/*
+						 * Environment is a class that contain Exit method
+						 * this way the loop ends
+						*/
+						break;
+					default:
+						Console.WriteLine("Please enter number from 1 to 5 only");
+						break;
+				}
 			}
-
-			
 		}
 		private static void AddTask()
 		{
 			Console.WriteLine("Enter task title");
+			string taskTitle = Console.ReadLine();
+
+			tasks[taskIndex] = taskTitle;
+			Console.WriteLine("Task added");
+
+			taskIndex++;
+		}
+		private static void ViewTasks()
+		{
+			Console.WriteLine("Task list: ");
+
+            for (int i = 0; i < taskIndex; i++)
+            {
+				if(taskIndex==0)
+					Console.WriteLine($"{i + 1}. Task Index is Empty!!");
+				else
+					Console.WriteLine($"{i + 1}. Task title is: {tasks[i]}");
+			}
+        }
+		private static void MarkComplete()
+		{
+			Console.WriteLine("Enter task number: ");
+
+			int taskNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+			int taskId = Convert.ToInt32(taskNumber);
+
+			tasks[taskId] = tasks[taskId] + "--COMPLETED";
+		}
+		private static void DeleteTask()
+		{
+			Console.WriteLine("Enter task number to delete: ");
+
+			int taskId = Convert.ToInt32(Console.ReadLine()) -1;
+
+			//tasks[taskId] = "";
+			tasks[taskId] = string.Empty;
 		}
 	}
 }
